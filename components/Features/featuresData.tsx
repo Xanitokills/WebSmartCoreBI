0f63108d74095878c70f3a8bae1bdf94dfe5c43e
Xanitokills <63894835+Xanitokills@users.noreply.github.com>
2024-11-22 23:08:09 -0500

    first commit

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Automatización de Procesos Empresariales",
+    description:
+      "¡Haz crecer tu negocio con la automatización hoy mismo! Si estás listo para llevar la eficiencia operativa de tu empresa al siguiente nivel, contáctanos y descubre cómo podemos ayudarte a transformar tus procesos empresariales.",
+  },
+  {
+    id: 2, 
+    icon: "/images/icon/icon-02.svg",
+    title: "Chatbots y Asistentes Virtuales para Atención al Cliente",
+    description:
+      "¡Haz que tu atención al cliente sea más eficiente! Descubre cómo los chatbots y asistentes virtuales pueden revolucionar la manera en que interactúas con tus clientes. Contáctanos para obtener una demostración o para conocer cómo esta tecnología puede beneficiar a tu empresa.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Análisis Predictivo para Optimización de Negocios",
+    description:
+      "Empieza a predecir el futuro de tu negocio hoy! No esperes a que los problemas surjan. Con nuestras soluciones de análisis predictivo, puedes tomar decisiones basadas en datos sólidos y anticipar el futuro de tu empresa.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "Detección y Prevención de Fraudes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 5,
+    icon: "/images/icon/icon-05.svg",
+    title: "Personalización de la Experiencia del Cliente",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 6,
+    icon: "/images/icon/icon-06.svg",
+    title: "Plataformas de Gestión y Análisis de Datos",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+];
+
+export default featuresData;
---
+++ components/FeaturesTab/featuresTabData.tsx
@@ -0,0 +1,30 @@
+import { FeatureTab } from "@/types/featureTab";
+
+const featuresTabData: FeatureTab[] = [
+  {
+    id: "tabOne",
+    title: "Consolidación de Datos en Tiempo Real.",
+    desc1: `Centraliza la información de múltiples fuentes en un solo lugar para obtener una visión global de tu negocio.`,
+    desc2: ``,
+    image: "/images/features/features-light-01.png",
+    imageDark: "/images/features/features-dark-01.svg",
+  },
+  {
+    id: "tabTwo",
+    title: "Automatización de Procesos",
+    desc1: `Mejora la eficiencia operativa automatizando tareas repetitivas y optimizando el flujo de trabajo de tus proyectos`,
+    desc2: `    Nam id eleifend dui, id iaculis purus. Etiam lobortis neque nec finibus sagittis. Nulla ligula nunc egestas ut.`,
+    image: "/images/features/features-light-01.png",
+    imageDark: "/images/features/features-dark-01.svg",
+  },
+  {
+    id: "tabThree",
+    title: "Functional Blog, DB, Auth and Many More",
+    desc1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies lacus non fermentum ultrices. Fusce consectetur le.`,
+    desc2: `Nam id eleifend dui, id iaculis purus. Etiam lobortis neque nec finibus sagittis. Nulla ligula nunc egestas ut.`,
+    image: "/images/features/features-light-01.png",
+    imageDark: "/images/features/features-dark-01.svg",
+  },
+];
+
+export default featuresTabData;
